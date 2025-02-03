1cc526c3c33318a61a27587cac71f62b716620ff
fmattaperdomo <fmattaperdomo@gmail.com>
2025-02-02 21:34:27 -0500

    Started Project Arka

---
+++ src/main/java/com/fmattaperdomo/ecommerce_arka/EcommerceArkaApplication.java
@@ -0,0 +1,13 @@
+package com.fmattaperdomo.ecommerce_arka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceArkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceArkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmattaperdomo/ecommerce_arka/EcommerceArkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmattaperdomo.ecommerce_arka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceArkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
